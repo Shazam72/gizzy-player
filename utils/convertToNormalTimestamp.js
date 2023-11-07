a6cb8f1fc124724c15852aac38fe0ddbb2f68b8a
Culture Code <jfrancoiszouba72@gmail.com>
2023-11-07 02:42:39 +0100

    Adding leading zeros for duration

--- utils/convertToNormalTimestamp.js
+++ utils/convertToNormalTimestamp.js
@@ -1,5 +1,8 @@
 export default (timeInSeconds = 0) => {
-  const minutes = Math.floor(timeInSeconds / 60);
-  const seconds = Math.round(timeInSeconds % 60);
+  let minutes = Math.floor(timeInSeconds / 60);
+  let seconds = Math.round(timeInSeconds % 60);
+
+  if (seconds.toString().length == 1) seconds = "0" + seconds;
+
   return `${minutes}:${seconds}`;
 };
